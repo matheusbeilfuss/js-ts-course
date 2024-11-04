b54b486da61109c7879f67a75094f8e5b27575f1
Beilfuss <matheusdicimb@gmail.com>
2024-11-04 08:01:08 +0300

    Link vs a

---
+++ 15-react-router-dom-v6/04-link-vs-a/04-link-vs-a.md
@@ -0,0 +1,7 @@
+# Usando Links: Link vs a
+
+- Criação de um componente "Menu" com links;
+- O menu não fica como uma rota, porque aparece em todas as páginas, ele fica direto dentro do BrowserRouter;
+- Usando o "<a></a>", toda vez que se clica em um link, a página é atualizada;
+  - Interessante na ação de logout.
+- Para não acontecer essa atualização, para só trocar os componentes da página carregando estados, é interessante usar o Link.
---
+++ 15-react-router-dom-v6/04-link-vs-a/vite-react-ts/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import { Home } from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from "./components/About";
+import { Menu } from "./components/Menu";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
