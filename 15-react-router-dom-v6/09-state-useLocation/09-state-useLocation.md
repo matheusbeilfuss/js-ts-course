22c9cf41fc944ff8246c371416c020a272bf2491
Beilfuss <matheusdicimb@gmail.com>
2024-11-10 14:02:00 +0300

    state e useLocation

---
+++ 15-react-router-dom-v6/09-state-useLocation/09-state-useLocation.md
@@ -0,0 +1,4 @@
+# state e useLocation: Enviando e recebendo estado
+
+- Pode-se usar o "useLocation" para enviar e receber estado nos componentes.
+  - Quando se clica em um link, pode-se levar estado de um lugar para outro.
---
+++ 15-react-router-dom-v6/09-state-useLocation/vite-react-ts/src/components/Post/index.tsx
@@ -0,0 +1,13 @@
+export const Post = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad temporibus
+        expedita, sapiente quisquam assumenda ipsum consequatur iusto! Adipisci,
+        consectetur, error unde hic culpa ex dicta nemo aliquid, excepturi
+        nesciunt iusto.
+      </p>
+    </div>
+  );
+};
